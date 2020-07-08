56b99a46fad30174ca698232dd97bdebe1741a5d
hejtmy <hejtmy@gmail.com>
2020-07-08 18:28:30 +0200

    Adds freezing report and freezing function calculation

---
+++ functions/analysis.R
@@ -0,0 +1,19 @@
+#' returns freezes and some summative stats
+#'
+#' @param obj avoidance.single object
+#'
+#' @return
+#' @export
+#'
+#' @examples
+analyze_freezing <- function(obj, min_duration, speed_threshold){
+  res <- list()
+  res$data <- collect_freezes(obj, min_duration = min_duration, 
+                                 speed_threshold = speed_threshold)
+  res$summary <- list(
+    n_freezes = length(res$data$time),
+    total_time = sum(res$data$duration),
+    time_ratio = sum(res$data$duration)/tail(obj$position$data$time_since_start, 1)
+  )
+  return(res)
+}
--- playgound.R
+++ playgound.R
@@ -4,3 +4,17 @@ source("functions/loading.R")
 data_folder <- file.path("data", "")
 
 obj <- load_data(data_folder)
+
+
+animal <- obj$hab1$females$animal_10
+plot_speed(animal) +
+  xlim(0,100)
+
+plot_path(animal)
+freezes <- collect_freezes(animal, min_duration = 2, speed_threshold = 1)
+str(freezes)
+freeze_times <- rbind(freezes$time, freezes$time+freezes$duration)
+
+plot_path(animal) +
+  geom_navr_path_events(animal$position, freeze_times)
+
